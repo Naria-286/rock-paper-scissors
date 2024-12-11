34aade3a904b7fbeb80f4ed4b76fd84f82b2e99a
Naria-286 <1885397556+Naria-286@users.noreply.github.com>
2024-12-10 20:40:37 -0700

    Create playGame function to play 5 rounds of RPS

--- script.js
+++ script.js
@@ -1,56 +1,85 @@
-let humanScore = 0;
-let computerScore = 0;
+function playGame () {
 
-function getComputerChoice () {
-    let random = Math.floor(Math.random() * 3) + 1
-    switch(random) {
-        case 1:
-            return "rock"
-        break
-        case 2:
-            return "paper"
-        break
-        case 3:
-            return "scissors"
-        break
-    }
-}
-function getHumanChoice () {
-    // let humanChoice = prompt("Your Choice: ")
-    let humanChoice = "RoCk"
-    return humanChoice.toLowerCase()
-}
-
-function checkWinner (humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return "Draw!"
-    } else if (humanChoice === "scissors"){
-        if (computerChoice === "rock"){
-            return "YOU LOSE: your scissors gets crushed by rock"
-        } else {
-            return "YOU WIN: your scissors cut paper"
-        }
-    } else if (humanChoice === "paper"){
-        if (computerChoice === "scissors"){
-            return "YOU LOSE: your paper gets cut"
-        } else {
-            return "YOU WIN: your paper wraps the rock"
-        }
-    } else if (humanChoice === "rock"){
-        if (computerChoice === "paper"){
-            return "YOU LOSE: your rock gets lose"
-        } else {
-            return "YOU WIN: your rock break scissors"
+    function getComputerChoice () {
+        let random = Math.floor(Math.random() * 3) + 1
+        switch(random) {
+            case 1:
+                computerSelection = "rock"
+            case 2:
+                computerSelection = "paper"
+            case 3:
+                computerSelection = "scissors"
         }
     }
+
+    //Temp "rock" placeholder
+    function getHumanChoice () {
+        let humanChoice = prompt("Your Choice: ")
+        // let humanChoice = "RoCk"
+        humanSelection = humanChoice
+    }
+
+    function checkWinner (humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            return "DRAW!"
+        } else if (humanChoice === "scissors"){
+            if (computerChoice === "rock"){
+                computerScore++
+                return "YOU LOSE: your scissors gets crushed by rock"
+            } else {
+                humanScore++
+                return "YOU WIN: your scissors cut paper"
+            }
+        } else if (humanChoice === "paper"){
+            if (computerChoice === "scissors"){
+                computerScore++
+                return "YOU LOSE: your paper gets cut"
+            } else {
+                humanScore++
+                return "YOU WIN: your paper wraps the rock"
+            }
+        } else if (humanChoice === "rock"){
+            if (computerChoice === "paper"){
+                computerScore++
+                return "YOU LOSE: your rock gets lose"
+            } else {
+                humanScore++
+                return "YOU WIN: your rock break scissors"
+            }
+        }
+    }
+
+    function playRound(humanChoice,computerChoice) {
+        humanChoice = humanChoice.toLowerCase()
+        console.log(checkWinner(humanChoice,computerChoice))
+        console.log(`HUMAN:${humanChoice} COMPUTER:${computerChoice} SCORE:${humanScore}-${computerScore}`)
+    }
+
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    let humanSelection, computerSelection
+
+    getHumanChoice()
+    getComputerChoice()
+    playRound(humanSelection,computerSelection)
+    //2
+    getHumanChoice()
+    getComputerChoice()
+    playRound(humanSelection,computerSelection)
+    //3
+    getHumanChoice()
+    getComputerChoice()
+    playRound(humanSelection,computerSelection)
+    //4
+    getHumanChoice()
+    getComputerChoice()
+    playRound(humanSelection,computerSelection)
+    //5
+    getHumanChoice()
+    getComputerChoice()
+    playRound(humanSelection,computerSelection)
 }
 
-function playRound(humanChoice,computerChoice) {
-    console.log(checkWinner(humanChoice,computerChoice))
-    console.log(`HUMAN:${humanChoice} COMPUTER:${computerChoice}`)
-}
-
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
-
-console.log(playRound(humanSelection,computerSelection) )
+playGame();
