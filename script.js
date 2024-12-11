05eed5ca44598e267dd549412a368de7bef199a9
Naria-286 <1885397556+Naria-286@users.noreply.github.com>
2024-12-10 20:12:26 -0700

    Create checkwinner function that returns a string

--- script.js
+++ script.js
@@ -21,12 +21,33 @@ function getHumanChoice () {
     return humanChoice.toLowerCase()
 }
 
-function checkWinner (a,b) {
-
+function checkWinner (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "Draw!"
+    } else if (humanChoice === "scissors"){
+        if (computerChoice === "rock"){
+            return "YOU LOSE: your scissors gets crushed by rock"
+        } else {
+            return "YOU WIN: your scissors cut paper"
+        }
+    } else if (humanChoice === "paper"){
+        if (computerChoice === "scissors"){
+            return "YOU LOSE: your paper gets cut"
+        } else {
+            return "YOU WIN: your paper wraps the rock"
+        }
+    } else if (humanChoice === "rock"){
+        if (computerChoice === "paper"){
+            return "YOU LOSE: your rock gets lose"
+        } else {
+            return "YOU WIN: your rock break scissors"
+        }
+    }
 }
 
 function playRound(humanChoice,computerChoice) {
-    return humanChoice + " " + computerChoice
+    console.log(checkWinner(humanChoice,computerChoice))
+    console.log(`HUMAN:${humanChoice} COMPUTER:${computerChoice}`)
 }
 
 const humanSelection = getHumanChoice()
